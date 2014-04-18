d453dac8534c9728ebf278d728d0aa4647e1db7e
Wilka Carvalho <wilka.carvalho@stonybrook.edu>
2014-04-18 14:42:31 -0400

    fixed printer

---
+++ headers/printer.h
@@ -0,0 +1,40 @@
+//------------------------------------------
+//		C++
+//------------------------------------------
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+
+//------------------------------------------
+//		ROOT
+//------------------------------------------
+#include "TMath.h"
+#include "TObject.h"
+#include "TTree.h"
+#include "TFile.h"
+#include "TGraph.h"
+#include "TH2D.h"
+
+//------------------------------------------
+//		mine
+//------------------------------------------
+#include "dirc_objects.h"
+#include "functions.h"
+//------------------------------------------
+//		Functions
+//------------------------------------------
+using namespace std;
+
+
+class Printer
+{
+public:
+  string Name;
+  int which;
+  string Hist_Title;
+};
+
+void Print_EventTH2D(Reconstruction* reconstruction, Printer printer, Displayer display);
+void Print_EventTH1D(Reconstruction* reconstruction, Printer printer, Displayer display);
+
