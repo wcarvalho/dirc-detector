cf94eb9dc91393c8394009f7342a216d37eae08b
Wilka Carvalho <wilka.carvalho@stonybrook.edu>
2014-11-22 17:52:56 -0500

    print_2D_vector deprecated

--- functions/Print_2D_Vector.C
+++ functions/Print_2D_Vector.C
@@ -16,7 +16,7 @@ void Print_2D_Vector(vector< vector<double> > array)
   for(i=0; i<array.size(); i++){
     cout << setw(17) << "(" << i << ")" << setw(5) << "|";
     for (j=0; j<array[0].size(); j++){
-    	printf ("%f \t|", 5, array[i][j]);
+    	// printf ("%f \t|", 5, array[i][j]);
     }
     cout << endl;
   }
