09cb4ee363d0fa9a451a15710d1c5ce257469a92
Wilka Carvalho <wilka.carvalho@stonybrook.edu>
2015-08-30 17:39:47 -0700

    fixed smearer, embedder build scripts..

--- build_scripts/buildparticleembedder.sh
+++ build_scripts/buildparticleembedder.sh
@@ -3,6 +3,6 @@
 cd scripts
 cd embedParticles
 
-g++ `LDFLAGS=-Wl,--no-as-needed` -std=c++11 -O3 -march=native -w -Wall embed_particles.cpp `pkg-config --libs DircObjs``root-config --libs` -I`root-config --incdir` `pkg-config --libs DircObjs` `pkg-config --cflags DircObjs` `pkg-config --libs SimObjs` -o $scripts/embed_particles
+g++ LDFLAGS=-Wl,--no-as-needed -std=c++11 -O3 -march=native -w -Wall embed_particles.cpp `pkg-config --libs DircObjs``root-config --libs` -I`root-config --incdir` `pkg-config --libs DircObjs` `pkg-config --cflags DircObjs` `pkg-config --libs SimObjs` -o $scripts/embed_particles
 
 cd ../..
